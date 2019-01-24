2cff143e2fe367c8c59801f9e29d5c06facfa9ca
RemiRuc <remi.rucojevic@gmail.com>
2019-01-24 16:06:22 +0100

    RetroFit implemented

---
+++ app/src/main/java/com/example/ditplme/retro/RetrofitInterface.java
@@ -0,0 +1,23 @@
+package com.example.ditplme.retro;
+
+
+
+import com.example.ditplme.Player;
+import com.example.ditplme.SubscriptionResponse;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.PUT;
+import retrofit2.http.Query;
+
+public interface RetrofitInterface {
+
+    @GET("bdd.json")
+    public Call<List<Player>> getAllPlayers();
+
+    @PUT("add.php")
+    Call<SubscriptionResponse> createPlayer(@Query(value = "pseudo", encoded = true) String pseudo, @Query(value = "mdp", encoded = true) String mdp);
+
+}
